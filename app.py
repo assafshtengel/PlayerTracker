8b0df35f6ae1fb12e8ba7438e7f2751e7e144beb
assafshtengel <socr.co.il@gmail.com>
2024-12-07 22:31:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import logging
+
+# הגדרת לוגים
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+print("Current directory:", os.getcwd())
+print("Templates:", os.listdir('templates'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    logger.debug("Home route was accessed.")
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
 
